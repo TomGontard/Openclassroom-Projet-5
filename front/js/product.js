432e7547861691478494f2c9e3feba58d603aeff
Tom Gontard <tomgontard@protonmail.com>
2024-08-14 15:22:44 +0200

    Ajout fonction récupération des informations du produit par l'API via son ID

--- front/js/product.js
+++ front/js/product.js
@@ -8,3 +8,16 @@ async function getProductIdFromURL() {
         return "Erreur";
     }
 }
+
+//Récupération des informations du produit par l'API via son ID
+async function getProductDetails() {
+    try {
+        const id = await getProductIdFromURL();
+        const response = await fetch(`http://localhost:3000/api/products/${id}`);
+        const product = await response.json();
+        console.log(product);
+        return product;
+    } catch (error) {
+        console.error('Erreur lors de la récupération des produits :', error);
+  }
+}
